efdbfcfb9a02380ecff33c1ba2bc87f3a1c85a49
semion_nikolaev <ivanhoekn@gmail.com>
2022-11-14 05:25:05 +0300

    OnlineStore. First Commit. Base Start App.

---
+++ src/main/java/proj3ct/onlinestore/config/BotInitializer.java
@@ -0,0 +1,27 @@
+package proj3ct.onlinestore.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import proj3ct.onlinestore.service.TelegramBot;
+
+@Component
+public class BotInitializer {
+    @Autowired
+    TelegramBot telegramBot;
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(telegramBot);
+        }
+        catch (TelegramApiException e) {
+
+        }
+    }
+}
